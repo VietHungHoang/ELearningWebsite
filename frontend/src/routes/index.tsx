a40f2c7fde09e79c00fe21454fc1eafc77dc6370
Hoàng Việt Hưng <viethunghoang1508@gmail.com>
2025-10-23 00:54:26 +0700

    chore: remove old project files

--- frontend/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import instructorRoutes from "./tutor.routes";
-
-const routes = [
-    ...instructorRoutes, 
-]
-
-export const router = createBrowserRouter(routes);
-
--- frontend/src/types/common.ts
+++
@@ -1,6 +0,0 @@
-// API Response wrapper interface matching Java backend
-export interface ApiResponse<T> {
-  status: number;
-  data: T;
-  message: string;
-}
