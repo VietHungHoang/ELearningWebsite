a7461d993965ab98fadc39355d783dff77ced037
HtienHung <thangngodan@gmail.com>
2025-10-25 03:03:25 +0700

    refactor: chuyển learner_service sang YAML config + thêm Eureka

--- learner_service/src/main/java/com/elearning/learner_service/LearnerServiceApplication.java
+++ learner_service/src/main/java/com/elearning/learner_service/LearnerServiceApplication.java
@@ -2,8 +2,10 @@ package com.elearning.learner_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LearnerServiceApplication {
 
 	public static void main(String[] args) {
