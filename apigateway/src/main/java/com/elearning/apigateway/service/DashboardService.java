56eafa9f6cafab94673115bb212b73130ae9545d
Hoang Tien Hung <136312056+Hercules3006@users.noreply.github.com>
2025-10-30 04:28:38 +0700

    Delete apigateway directory

--- apigateway/src/main/java/com/elearning/apigateway/service/DashboardService.java
+++
@@ -1,9 +0,0 @@
-package com.elearning.apigateway.service;
-
-
-import java.util.Map;
-
-public interface DashboardService {
-    Map<String, Object> getDashboard(Long accountId);
-}
-
--- apigateway/src/test/java/com/elearning/bff_service/BffServiceApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.elearning.bff_service;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BffServiceApplicationTests {
-
-}
-
