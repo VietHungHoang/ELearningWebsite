f7d0038e45db015b4ab7f45859c4700872483053
Hoàng Việt Hưng <viethunghoang1508@gmail.com>
2025-11-01 19:12:52 +0700

    fix (api-gateway) : convert to webflux

--- backend/gateway/api-gateway/src/main/java/com/elearning/apigateway/config/SecurityConfig.java
+++ backend/gateway/api-gateway/src/main/java/com/elearning/apigateway/config/SecurityConfig.java
@@ -3,25 +3,22 @@ package com.elearning.apigateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .authorizeExchange(exchanges -> exchanges
+                    .pathMatchers("/api/auth/**").permitAll()
+                    .anyExchange().authenticated()
             )
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(Customizer.withDefaults())
-            );
-        http.csrf(csrf -> csrf.disable());
+            .oauth2ResourceServer(oauth2 -> {
+                oauth2.jwt(Customizer.withDefaults());
+            });
         return http.build();
     }
 }
