b802ad6e014c0f8c9f4a97a2244e5e4454903479
Hoàng Việt Hưng <viethunghoang1508@gmail.com>
2025-11-03 20:59:42 +0700

    feat (api-gateway) : add config CORS

--- backend/gateway/api-gateway/src/main/java/com/elearning/apigateway/config/SecurityConfig.java
+++ backend/gateway/api-gateway/src/main/java/com/elearning/apigateway/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -20,5 +25,19 @@ public class SecurityConfig {
                 oauth2.jwt(Customizer.withDefaults());
             });
         return http.build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
