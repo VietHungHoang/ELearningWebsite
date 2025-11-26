7189a570c30c389d2a9753c2f7515701ed681901
Hoàng Việt Hưng <viethunghoang1508@gmail.com>
2025-11-26 12:55:09 +0700

    chore (common-service) : mapping for subject and category

--- backend/services/common-service/src/main/java/com/elearning/commonservice/entity/Category.java
+++ backend/services/common-service/src/main/java/com/elearning/commonservice/entity/Category.java
@@ -31,4 +31,7 @@ public class Category extends BaseEntity {
 
     @OneToMany(mappedBy = "parent")
     private List<Category> children;
+
+    @OneToMany(mappedBy = "category")
+    private List<Subject> subjects;
 }
