1dc813f52cee30f1835cca9b26342143d2d3c402
HtienHung <thangngodan@gmail.com>
2026-01-06 03:10:28 +0700

    fix(config): Update base URLs for API, WebSocket, and SSE services to use HTTP

--- frontend/lernen/src/services/sseService.ts
+++ frontend/lernen/src/services/sseService.ts
@@ -1,6 +1,6 @@
 import type { Notification } from '../types/notifications';
 
-const API_BASE_URL ='https://ec2-13-236-4-126.ap-southeast-2.compute.amazonaws.com:8081';
+const API_BASE_URL ='http://13.236.4.126:8081';
 
 interface SseConnectionOptions {
     onNotification: (notification: Notification) => void;
