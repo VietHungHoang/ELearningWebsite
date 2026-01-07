75f69bd2e872a7ad50c285174651d06808e5d142
Hoàng Việt Hưng <viethunghoang1508@gmail.com>
2026-01-07 22:39:31 +0700

    feat : add route gateway

--- backend/gateway/api-gateway/src/main/java/com/elearning/apigateway/routes/TutorRoutesConfig.java
+++ backend/gateway/api-gateway/src/main/java/com/elearning/apigateway/routes/TutorRoutesConfig.java
@@ -9,58 +9,53 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TutorRoutesConfig {
 
-    @Value("${services.tutor-service.url}")
-    private String tutorServiceUrl;
+        @Value("${services.tutor-service.url}")
+        private String tutorServiceUrl;
 
-    @Value("${services.bff-service.url}")
-    private String bffServiceUrl;
+        @Value("${services.bff-service.url}")
+        private String bffServiceUrl;
 
-    private final String prefix = "/api/v1/tutors/";
+        private final String prefix = "/api/v1/tutors/";
 
-    @Bean
-    public RouteLocator tutorServiceRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                // Direct routes
-                .route("tutor-availability", r -> r
-                        .path(
-                                prefix + "{tutorId}/availabilities",
-                                prefix + "{tutorId}/onboarding/**",
-                                prefix + "me/dashboard/stats",
-                                prefix + "me/earnings/**",
-                                "/api/v1/public/tutors/{tutorId}",
-                                "/api/v1/tutors/me/profile",
-                                "/api/v1/tutors/me/career-entries",
-                                "/api/v1/tutors/me/career-entries/educations",
-                                "/api/v1/tutors/me/career-entries/experiences",
-                                "/api/v1/tutors/me/career-entries/{id}",
-                                "/api/v1/admin/tutors/approve/{tutorId}",
-                                "/api/v1/tutors/zoom/oauth/authorize",
-                                "/api/v1/tutors/zoom/oauth/callback"
-                                )
-                        .filters(f -> f
-                                .rewritePath(
-                                        "/api/v1/public/(?<rest>.*)",
-                                        "/api/v1/${rest}"
-                                )
-                                .rewritePath(
-                                        "/api/v1/admin/(?<rest>.*)",
-                                        "/api/v1/${rest}"
-                                )
-                        )
-                        .uri(tutorServiceUrl))
+        @Bean
+        public RouteLocator tutorServiceRoutes(RouteLocatorBuilder builder) {
+                return builder.routes()
+                                // Direct routes
+                                .route("tutor-availability", r -> r
+                                                .path(
+                                                                prefix + "{tutorId}/availabilities",
+                                                                prefix + "{tutorId}/onboarding/**",
+                                                                prefix + "me/dashboard/stats",
+                                                                prefix + "me/earnings/**",
+                                                                "/api/v1/public/tutors/{tutorId}",
+                                                                "/api/v1/tutors/me/profile",
+                                                                "/api/v1/tutors/me/career-entries",
+                                                                "/api/v1/tutors/me/career-entries/educations",
+                                                                "/api/v1/tutors/me/career-entries/experiences",
+                                                                "/api/v1/tutors/me/career-entries/{id}",
+                                                                "/api/v1/admin/tutors/approve/{tutorId}",
+                                                                "/api/v1/tutors/zoom/oauth/authorize",
+                                                                "/api/v1/tutors/zoom/oauth/callback",
+                                                                "/api/v1/tutors/{tutorId}/reviews")
+                                                .filters(f -> f
+                                                                .rewritePath(
+                                                                                "/api/v1/public/(?<rest>.*)",
+                                                                                "/api/v1/${rest}")
+                                                                .rewritePath(
+                                                                                "/api/v1/admin/(?<rest>.*)",
+                                                                                "/api/v1/${rest}"))
+                                                .uri(tutorServiceUrl))
 
-                // Bff routes
-                .route("tutor-detail", r -> r
-                        .path("/api/v1/tutors/me/dashboard/charts")
-                        .filters(f -> f
-                                .rewritePath(
-                                        "/api/v1/tutors/(?<rest>.*)",
-                                        "/api/v1/bff/tutors/${rest}"
-                                )
-                        )
+                                // Bff routes
+                                .route("tutor-detail", r -> r
+                                                .path("/api/v1/tutors/me/dashboard/charts")
+                                                .filters(f -> f
+                                                                .rewritePath(
+                                                                                "/api/v1/tutors/(?<rest>.*)",
+                                                                                "/api/v1/bff/tutors/${rest}"))
 
-                        .uri(bffServiceUrl))
+                                                .uri(bffServiceUrl))
 
-                .build();
-    }
+                                .build();
+        }
 }
